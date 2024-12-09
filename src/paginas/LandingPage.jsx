4ad71643ff7d365c50834269f02481eb4e0f2875
pduran25 <43051206+pduran25@users.noreply.github.com>
2024-12-09 15:23:57 -0500

    oka

--- src/paginas/LandingPage.jsx
+++ src/paginas/LandingPage.jsx
@@ -1,85 +1,139 @@
 import React, { useState } from 'react';
+import sound from '/assets/chiverito.mp3';
+import styled from 'styled-components';
 import backgroundImage from '../assets/fondofinal.png';
+import nombreapellido from '../assets/nombreapellido.png';
+import celular from '../assets/celular.png';
+import cuenta from '../assets/instagram.png';
+import ciudad from '../assets/ciudad.png';
+import btnenviar from '../assets/btnenviar.png';
+import regalo from '../assets/regalo.png';
+import base from '../assets/base.png';
 import logoImage from '../assets/logoImage.png';
 import parteImage from '../assets/parteImage.png';
 
+
+
 const LandingPage = () => {
+
+  const [enviadoExitosamente, setEnviadoExitosamente] = useState(false);
+ // const history = useHistory();
   const [formData, setFormData] = useState({
     nombres: '',
     celular: '',
-    cuenta: '',
+    cuenta: '',// Valor predeterminado
     ciudad: '', 
     animacion: 0
   });
+  var source = "";
+  var source2 = "";
 
-  var source = "./assets/guachosorpresa.glb";
-  var source2 = "./assets/guachosorpresa.usdz";
+  const search = window.location.search;
+  const params = new URLSearchParams(search);
+  const codigo = params.get('animacion');
+  const paso = params.get('paso');
+  console.log("animacion numero: "+codigo);
+
+
+
+  source = "./assets/guachosorpresa.glb";
+  source2 = "./assets/guachosorpresa.usdz";
+
+
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+
+  const toggleAudio = () => {
+    const audioElement = document.getElementById('myAudio');
+  
+    if (isAudioPlaying) {
+      audioElement.pause();
+    } else {
+      audioElement.play();
+    }
+  
+    setIsAudioPlaying(!isAudioPlaying);
+  };
+
+
+
+
 
   return (
-    <div style={{
-      background: `url(${backgroundImage}) repeat fixed center center / cover`,
-      height: '100%',
-      justifyContent: 'center',
-      flexDirection: 'column',
-      alignItems: 'center',
-    }}>
+    
+      <>
+        <div style={{
+          background: `url(${backgroundImage}) repeat fixed center center / cover`,
+          height: '100%',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}>
       <div style={{ marginBottom: '20px' }}>
         <div style={{ marginBottom: '20px' }}>
-          <img
-            src={logoImage}
-            alt="Logo"
-            style={{ width: '100vw' }}
-          />
-        </div>
-        <h1 style={{ textAlign: 'center', color: '#FFF' }}>¡Envío Exitoso!</h1>
-        <p style={{ textAlign: 'center', color: '#FFF' }}>Gracias por registrarte. Tomate la foto con Guachito y súbelo a tu Instagram.</p>
-        <div className="App" style={{ width: '100%', height: '100%' }}>
-          <model-viewer
-            src={source}
-            ios-src={source2}
-            camera-controls
-            camera-orbit="-40deg 70deg 200m"
-            camera-target="0 0 0"
-            ar
-            ar-modes="scene-viewer webxr quick-look"
-            xr-environment
-            ar-placement="wall"
-            autoplay
-          >
-            <Boton slot="ar-button">
-              Ingresa
-            </Boton>
-          </model-viewer>
-        </div>
+      <img
+        src={logoImage}
+        alt="Logo"
+        style={{ width: '100vw' }}
+      />
       </div>
-    </div>
+        <h1 style={{ textAlign: 'center', color: '#FFF' }}>¡Envío Exitoso!</h1>
+        <p style={{ textAlign: 'center', color: '#FFF'  }}>Gracias por registrarte. Tomate la foto con Guachito y súbelo a tu Instagram.</p>
+        <div className="App" style={{ width: '100%', height: '100%' }}>
+        <model-viewer
+          src={source}
+          ios-src={source2}
+          camera-controls
+          camera-orbit="-40deg 70deg 200m"
+          camera-target="0 0 0"
+          ar
+          ar-modes="scene-viewer webxr quick-look"
+          xr-environment
+          ar-placement="wall"
+          autoplay
+        >
+
+
+          <Boton slot="ar-button">
+            Ingresa
+          </Boton>
+        </model-viewer>
+      </div>
+      </div></div>
+      </>
+    
+    
   );
 };
 
-const Boton = styled.button`
-  display: inline-block;
-  border: none;
-  border-radius: 4px;
-  background-color: #006a22;
-  color: #fff;
-  width: 70%;
-  margin: 15%;
-  margin-top: 0;
-  font-size: 16px;
-  font-weight: bold;
-  text-align: center;
-  text-decoration: none;
-  cursor: pointer;
-  alignItems: 'center',
+export default LandingPage;
 
-  &:hover {
-    background-color: #006a22;
-  }
-
-  &:focus {
-    outline: none;
-    background-color: #006a22;
-  }
+const SoundButton = styled.img`
+display: inline-block;
 `;
 
-export default LandingPage;
+const Boton = styled.button`
+
+display: inline-block;
+border: none;
+border-radius: 4px;
+background-color: #006a22;
+color: #fff;
+width: 70%;
+margin: 15%;
+margin-top: 0;
+font-size: 16px;
+font-weight: bold;
+text-align: center;
+text-decoration: none;
+cursor: pointer;
+alignItems: 'center',
+
+&:hover {
+  background-color: #006a22;
+}
+
+&:focus {
+  outline: none;
+  background-color: #006a22;
+}
+`;
