32dee80ebc0ba7f998e7893d535ef63a0386c3d8
pduran25 <43051206+pduran25@users.noreply.github.com>
2024-12-10 15:12:10 -0500

    Actualización para incluir todos los archivos

--- src/paginas/LandingPage.jsx
+++ src/paginas/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import backgroundImage from '../assets/fondocontecon.png';
+import backgroundImage from '../assets/fondoimagen.png';
 import logoImage from '../assets/logotipoconte.png';
 
 
