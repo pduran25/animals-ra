78149a92ff72592604d3d1586111af19868ef2bc
pduran25 <43051206+pduran25@users.noreply.github.com>
2024-12-10 15:42:29 -0500

    oka

--- src/paginas/LandingPage.jsx
+++ src/paginas/LandingPage.jsx
@@ -17,8 +17,8 @@ const LandingPage = () => {
 
 
 
-  source = "./assets/cubo10.glb";
-  source2 = "./assets/cubo10.usdz";
+  source = "./assets/cub11.glb";
+  source2 = "./assets/cub11.usdz";
 
 
   const [isAudioPlaying, setIsAudioPlaying] = useState(false);
