4760d60c5b7fb1966801d3aae1f1b453ac5c688d
pduran25 <43051206+pduran25@users.noreply.github.com>
2024-12-09 23:51:55 -0500

    ok

--- src/paginas/LandingPage.jsx
+++ src/paginas/LandingPage.jsx
@@ -68,6 +68,7 @@ const LandingPage = () => {
           camera-target="0 0 0"
           ar
           ar-modes="scene-viewer webxr quick-look"
+          exposure="1.2"
           xr-environment
           ar-placement="wall"
           autoplay
