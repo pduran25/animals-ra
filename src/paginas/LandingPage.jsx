b6e71085313bf07615042ab3cd35b2718416151b
pduran25 <43051206+pduran25@users.noreply.github.com>
2024-12-10 22:40:23 -0500

    cubo 12

--- src/paginas/LandingPage.jsx
+++ src/paginas/LandingPage.jsx
@@ -17,8 +17,8 @@ const LandingPage = () => {
 
 
 
-  source = "./assets/cub11.glb";
-  source2 = "./assets/cub11.usdz";
+  source = "./assets/cub12.glb";
+  source2 = "./assets/cub12.usdz";
 
 /*ok*/
   const [isAudioPlaying, setIsAudioPlaying] = useState(false);
