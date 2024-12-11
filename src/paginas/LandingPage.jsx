93e6539756a65f4e720f8a93d1cc181b37f3c79f
pduran25 <43051206+pduran25@users.noreply.github.com>
2024-12-10 23:22:17 -0500

    ok

--- src/paginas/LandingPage.jsx
+++ src/paginas/LandingPage.jsx
@@ -68,7 +68,7 @@ const LandingPage = () => {
       />
       </div>
         <h1 style={{ textAlign: 'center', color: 'white', fontSize: '3em', WebkitTextStroke: '2px darkgray'  }}>BIENVENIDOS</h1>
-        <p style={{ textAlign: 'center', color: 'white', fontSize: '1em', WebkitTextStroke: '1px darkgray' }}>EXPERIMENTA LA SENSACIÓN DEL FRIO CON ESTE CUBO DE HIELO GIGANTE! DESCUBRE COMO LA REALIDAD AUMENTADA PUEDE TRANSFORMAR TU ALREDEDOR!</p>
+        <p style={{ textAlign: 'center', color: 'white', fontSize: '1.5em', WebkitTextStroke: '1px darkgray' }}>EXPERIMENTA LA SENSACIÓN DEL FRIO CON ESTE CUBO DE HIELO GIGANTE! DESCUBRE COMO LA REALIDAD AUMENTADA PUEDE TRANSFORMAR TU ALREDEDOR!</p>
         <div className="App" style={{ width: '100%', height: '100%', marginTop: '50px' }}>
         <model-viewer
           src={source}
